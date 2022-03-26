d4615302bb749c7372ddbcdbfa8abc373431d33e
Vasiliy Manilo <manilo@ukr.net>
2022-03-26 21:02:56 +0200

    style: :sparkles: add basic design layout with daisyui, get posts from graphqlcms

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,61 @@
 	import '../app.css';
 </script>
 
+<div class="navbar bg-base-100 shadow-2xl">
+	<div class="flex-none">
+		<div class="navbar-start">
+			<div class="dropdown">
+				<label tabindex="0" class="btn btn-ghost btn-circle">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h7"
+						/></svg
+					>
+				</label>
+				<ul
+					tabindex="0"
+					class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+				>
+					<li><a href="/">Homepage</a></li>
+					<li><a href="/">Portfolio</a></li>
+					<li><a href="/">About</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="flex-1">
+		<a href="/" class="btn btn-ghost normal-case text-xl">Korsun Online</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal p-0">
+			<li><a href="/">Item 1</a></li>
+			<li tabindex="0">
+				<a href="/">
+					Parent
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						width="20"
+						height="20"
+						viewBox="0 0 24 24"
+						><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg
+					>
+				</a>
+				<ul class="p-2 bg-base-100">
+					<li><a href="/">Submenu 1</a></li>
+					<li><a href="/">Submenu 2</a></li>
+				</ul>
+			</li>
+			<li><a href="/">Item 3</a></li>
+		</ul>
+	</div>
+</div>
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export const load = async ({ fetch }) => {
+		const response = await fetch('/posts.json');
+
+		if (response.ok) {
+			const { posts } = await response.json();
+
+			return {
+				props: { posts }
+			};
+		}
+	};
+</script>
+
+<script>
+	import { format, formatDistance, subDays } from 'date-fns';
+	import uk from 'date-fns/locale/uk';
+
+	export let posts;
+</script>
+
+<svelte:head>
+	<title>Blog | Korsun Online</title>
+</svelte:head>
+
+<div class="hero bg-slate-600">
+	<div class="hero-overlay bg-opacity-60" />
+	<div class="hero-content text-center text-neutral-content">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+			<p class="mb-5">
+				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			</p>
+			<button class="btn btn-success btn-outline">Get Started</button>
+		</div>
+	</div>
+</div>
+
+<div class="container mx-auto p-5">
+	<div class="flex items-center">
+		{#each posts as { title, slug, date, excerpt, coverImage, featured, content }}
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<figure class="px-10 pt-10">
+					<img class="rounded w-full" src={coverImage.url} alt={`Cover image for ${title}`} />
+				</figure>
+				<div class="card-body">
+					<h2 class="card-title">{title} - {date}</h2>
+					<p>{content.html}</p>
+					<div class="card-actions justify-end">
+						<a href={slug} class="btn btn-success">Read post</a>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
